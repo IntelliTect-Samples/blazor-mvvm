d91d789b93a094f1386dee0f490fca34d455e97c
Kelly Adams <kelly@intellitect.com>
2023-01-14 13:57:40 -0800

    Make a viewmodel to handle getting data for FetchData view. - First passing test!

---
+++ src/Client/ViewModels/FetchDataViewModel.cs
@@ -0,0 +1,26 @@
+﻿using IntelliTect.Example.BlazorMvvm.Shared;
+using System.Collections.ObjectModel;
+using System.Net.Http.Json;
+
+namespace IntelliTect.Example.BlazorMvvm.Client.ViewModels;
+
+public partial class FetchDataViewModel : ViewModelBase
+{
+    private readonly HttpClient _httpClient;
+
+    public FetchDataViewModel(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    [ObservableProperty]
+    private ObservableCollection<WeatherForecast> _weatherForecasts = new();
+
+    public override async Task Loaded()
+    {
+        if (await _httpClient.GetFromJsonAsync<WeatherForecast[]>("WeatherForecast") is { } result)
+        {
+            WeatherForecasts = new ObservableCollection<WeatherForecast>(result);
+        }
+    }
+}
