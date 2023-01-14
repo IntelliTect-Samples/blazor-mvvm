a32df26782a381430ea8e163e01427d8a02ac3eb
Kelly Adams <kelly@intellitect.com>
2023-01-14 08:36:25 -0800

    FNP with tests (#1)

---
+++ src/Server/Controllers/WeatherForecastController.cs
@@ -0,0 +1,34 @@
+using IntelliTect.Example.BlazorMvvm.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IntelliTect.Example.BlazorMvvm.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
