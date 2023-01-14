28f2a0be5670de3cc56a232ff2301b2b5e4b5290
Kelly Adams <kelly@intellitect.com>
2023-01-14 12:33:57 -0800

    First stab at a general impl of ViewModels and Observable models. (#2)

---
+++ src/Client/ViewModels/IViewModelBase.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel;
+
+namespace IntelliTect.Example.BlazorMvvm.Client.ViewModels;
+
+public interface IViewModelBase : INotifyPropertyChanged
+{
+    Task OnInitializedAsync();
+    Task Loaded();
+}
---
+++ src/Client/ViewModels/ViewModelBase.cs
@@ -0,0 +1,19 @@
+﻿using CommunityToolkit.Mvvm.Input;
+
+namespace IntelliTect.Example.BlazorMvvm.Client.ViewModels;
+
+public abstract partial class ViewModelBase : ObservableObject, IViewModelBase
+{
+    public virtual async Task OnInitializedAsync()
+    {
+        await Loaded().ConfigureAwait(true);
+    }
+
+    protected virtual void NotifyStateChanged() => OnPropertyChanged((string?)null);
+
+    [RelayCommand]
+    public virtual async Task Loaded()
+    {
+        await Task.CompletedTask.ConfigureAwait(false);
+    }
+}
