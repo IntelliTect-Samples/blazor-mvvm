38d0f8e0b6509a8e1a6e556a29110d2ce7b5d7ed
Kelly Adams <kelly@intellitect.com>
2023-01-14 20:11:14 -0800

    Set up page with two components that send messages to each other.

--- src/Client/ViewModels/Dependencies.cs
+++ src/Client/ViewModels/Dependencies.cs
@@ -5,6 +5,8 @@ public static class Dependencies
     public static IServiceCollection AddViewModels(this IServiceCollection services)
     {
         services.AddTransient<FetchDataViewModel>();
+        services.AddTransient<HexEntryViewModel>();
+        services.AddTransient<TextEntryViewModel>();
 
         return services;
     }
---
+++ src/Client/ViewModels/HexEntryViewModel.cs
@@ -0,0 +1,35 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+using IntelliTect.Example.BlazorMvvm.Shared;
+
+namespace IntelliTect.Example.BlazorMvvm.Client.ViewModels;
+
+public partial class HexEntryViewModel : RecipientViewModelBase<ConvertAsciiToHexMessage>
+{
+    [ObservableProperty]
+    private string? _hexText;
+
+    public override void Receive(ConvertAsciiToHexMessage message)
+    {
+        var charArray = message.AsciiToConvert.ToCharArray();
+        var hexOutput = string.Empty;
+        foreach (var @char in charArray)
+        {
+            hexOutput += $"{Convert.ToInt32(@char):X}";
+        }
+
+        HexText = hexOutput;
+    }
+
+    public override Task Loaded()
+    {
+        IsActive = true;
+        return base.Loaded();
+    }
+
+    [RelayCommand]
+    public virtual void SendToAsciiConverter()
+    {
+        Messenger.Send(new ConvertHexToAsciiMessage(HexText ?? string.Empty));
+    }
+}
