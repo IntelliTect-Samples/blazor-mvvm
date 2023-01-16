63e14974685983aac405eec324449a4de90ebdbd
Kelly Adams <kelly@intellitect.com>
2023-01-15 20:10:37 -0800

    Forms with validation (#5)
    * Validtion WIP
    
    * Working validation

---
+++ src/Client/ViewModels/EditContactViewModel.cs
@@ -0,0 +1,27 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using IntelliTect.Example.BlazorMvvm.Shared;
+
+namespace IntelliTect.Example.BlazorMvvm.Client.ViewModels;
+
+public partial class EditContactViewModel : ViewModelBase
+{
+    [ObservableProperty]
+    private ContactInfo _contact = new();
+
+    [RelayCommand]
+    private void Save()
+    {
+        Contact.Validate();
+        if (Contact.HasErrors)
+            Console.WriteLine("After validating, errors found!");
+        else
+            Console.WriteLine("Sending contact to server!");
+    }
+
+
+    [RelayCommand]
+    protected void ClearForm()
+    {
+        Contact = new ContactInfo();
+    }
+}
